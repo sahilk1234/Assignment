4a280ee057c4da3ab3409273ea523c26ba0d2cc2
sahilk1234 <49304308+sahilk1234@users.noreply.github.com>
2021-09-24 20:29:04 +0530

    Add files via upload

--- assignment 4/script.js
+++ assignment 4/script.js
@@ -1,61 +1,92 @@
 var slideIndex = 1;
 var myTimer;
 var slideshowContainer;
-window.addEventListener("load",function() {
-    showSlides(slideIndex);
-    myTimer = setInterval(function(){plusSlides(1)}, 4000)
-    slideshowContainer = document.getElementsByClassName('carousel-container')[0];
-    slideshowContainer.addEventListener('mouseenter', pause)
-    slideshowContainer.addEventListener('mouseleave', resume)
-})
+window.addEventListener("load", function () {
+  showSlides(slideIndex);
+  myTimer = setInterval(function () {
+    plusSlides(1);
+  }, 4000);
+  slideshowContainer = document.getElementsByClassName("carousel-container")[0];
+  slideshowContainer.addEventListener("mouseenter", pause);
+  slideshowContainer.addEventListener("mouseleave", resume);
+});
 
-    function plusSlides(n){
-      clearInterval(myTimer);
-      if (n < 0){
-        showSlides(slideIndex -= 1);
-      } else {
-       showSlides(slideIndex += 1); 
-      }
-        
-      if (n === -1){
-        myTimer = setInterval(function(){plusSlides(n + 2)}, 4000);
-      } else {
-        myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
-      }
-    }
-
-function currentSlide(n){
+function plusSlides(n) {
   clearInterval(myTimer);
-  myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
-  showSlides(slideIndex = n);
+  if (n < 0) {
+    showSlides((slideIndex -= 1));
+  } else {
+    showSlides((slideIndex += 1));
+  }
+
+  if (n === -1) {
+    myTimer = setInterval(function () {
+      plusSlides(n + 2);
+    }, 4000);
+  } else {
+    myTimer = setInterval(function () {
+      plusSlides(n + 1);
+    }, 4000);
+  }
 }
 
-function showSlides(n){
+function currentSlide(n) {
+  clearInterval(myTimer);
+  myTimer = setInterval(function () {
+    plusSlides(n + 1);
+  }, 4000);
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("carousel-item");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace("active", "");
+    dots[i].className = dots[i].className.replace("active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
 
 pause = () => {
   clearInterval(myTimer);
-}
+};
 
-resume = () =>{
+resume = () => {
   clearInterval(myTimer);
-  myTimer = setInterval(function(){plusSlides(slideIndex)}, 4000);
+  myTimer = setInterval(function () {
+    plusSlides(slideIndex);
+  }, 4000);
+};
+function close() {
+  document.getElementsByClassName('menu')[0].setAttribute("style","visibility: hidden");
 }
 
+function close() {
+  var x = document.getElementsByClassName("menu");
+  if (x[0].style.display === "none") {
+    x[0].style.display = "block";
+  } else {
+    x[0].style.display = "none";
+  }
+}
 function myFunction() {
-
-  document.getElementById("sign-in-btn").style.display="block";
+  // document.getElementsByClassName('menu')[0].setAttribute("style", "display:block");
+  var x = document.getElementsByClassName("menu");
+  if (x[0].style.display === "none") {
+    x[0].style.display = "block";
+  } else {
+    x[0].style.display = "none";
+  }
 }
+
