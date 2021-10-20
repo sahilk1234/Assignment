bcb3bbbb587226e1419070c5aa9ded6b8790e00f
sahilk1234 <49304308+sahilk1234@users.noreply.github.com>
2021-10-20 22:40:43 +0530

    Delete question1.js

--- question1.js
+++
@@ -1,8 +0,0 @@
-// question 1
-
-function employee(firstName, lastName) {
-    var output = firstName + " " + lastName;
-    console.log(`${output}`);
-}
-console.log("question-1:");
-employee("John", "Doe");
